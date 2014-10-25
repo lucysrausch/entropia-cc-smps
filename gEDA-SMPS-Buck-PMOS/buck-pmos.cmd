a869f141ae0684742d492be5aec947ff1397018b
Ken Sarkies <ksarkies@internode.on.net>
2014-10-25 23:52:55 +1030

    New PCB for Buck SMPS with PMOS boost

---
+++ gEDA-SMPS-Buck-PMOS/buck-pmos.cmd
@@ -0,0 +1,74 @@
+# Pin name action command file
+
+# Start of element Q3
+ChangePinName(Q3, 1, G)
+ChangePinName(Q3, 1, G)
+ChangePinName(Q3, 1, G)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element Q2
+ChangePinName(Q2, E, E)
+ChangePinName(Q2, C, C)
+ChangePinName(Q2, B, B)
+
+# Start of element Q1
+ChangePinName(Q1, E, E)
+ChangePinName(Q1, C, C)
+ChangePinName(Q1, B, B)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, 2, -)
+ChangePinName(C3, 1, +)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element CONN2
+ChangePinName(CONN2, 2, 2)
+ChangePinName(CONN2, 1, 1)
+
+# Start of element CONN1
+ChangePinName(CONN1, 2, 2)
+ChangePinName(CONN1, 1, 1)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element C1
+ChangePinName(C1, 2, -)
+ChangePinName(C1, 1, +)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element L1
+ChangePinName(L1, 1, 1)
+ChangePinName(L1, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, 5, COMP)
+ChangePinName(U1, 6, VCC)
+ChangePinName(U1, 7, IPK)
+ChangePinName(U1, 8, COLL_D)
+ChangePinName(U1, 4, GND)
+ChangePinName(U1, 3, CT)
+ChangePinName(U1, 2, EMT_S)
+ChangePinName(U1, 1, COLL_S)
